cbbafb415c362d67144946b0d498e832c605b8b3
abhisathvik <151656430+abhisathvik@users.noreply.github.com>
2024-10-05 00:37:56 +0530

    Created index.ts CTF-3

---
+++ CTF-3/index.ts
@@ -0,0 +1,35 @@
+import * as algosdk from "algosdk";
+import { my_mnemonic as mnemonic } from "../config";
+
+(async () => {
+  const account = algosdk.mnemonicToSecretKey(mnemonic);
+
+  const algodClient = new algosdk.Algodv2(
+    "a".repeat(64),
+    "https://testnet-api.algonode.cloud",
+    443
+  );
+
+  const secretRecipient =
+    "2JAZQO6Z5BCXFMPVW2CACK2733VGKWLZKS6DGG565J7H5NH77JNHLIIXLY";
+
+  const encoder = new TextEncoder();
+
+  const payTxn = algosdk.makePaymentTxnWithSuggestedParamsFromObject({
+    sender: account.addr,
+    receiver: secretRecipient,
+    amount: algosdk.algosToMicroalgos(1),
+    suggestedParams: await algodClient.getTransactionParams().do(),
+    note: encoder.encode(
+      "b966c3d07e17b9442740dd2386e6e1ab191d51e964cee3b4dfc122f0fa865d10"
+    ),
+  });
+
+  const signedPayTxn = payTxn.signTxn(account.sk);
+
+  await algodClient.sendRawTransaction(signedPayTxn).do();
+
+  const res = await algosdk.waitForConfirmation(algodClient, payTxn.txID(), 3);
+
+  console.log(res);
+})();
